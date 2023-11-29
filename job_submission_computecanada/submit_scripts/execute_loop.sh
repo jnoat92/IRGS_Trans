b529d9dca3c7c68a6c8ca825018aae189800c4c6
Noa-graham <jnoat92@gmail.com>
2023-11-29 18:05:06 -0500

    s

--- job_submission_computecanada/submit_scripts/execute_loop.sh
+++ job_submission_computecanada/submit_scripts/execute_loop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-exp=1   # multi-stage-cnn                 time --> 5.5 min per epoch (02:55:00 for 50 epochs)     training!  graham
+# exp=1   # multi-stage-cnn                 time --> 5.5 min per epoch (02:55:00 for 50 epochs)     training!  graham (def-dclausi)
 
-# exp=2   # End-to-end loss-transformer     time --> 1.236hrs per epoch (65:00:00 for 50 epochs)     
+# exp=2   # End-to-end loss-transformer     time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    
 
 # exp=3   # multi-stage-transformer         time --> 1.236hrs per epoch (65:00:00 for 50 epochs)     
 
 # exp=4   # multi-stage-loss-end-to-end     time --> 1.236hrs per epoch (65:00:00 for 50 epochs)     
 
-# exp=5   # End-to-end loss-end-to-end      time --> 1.236hrs per epoch (65:00:00 for 50 epochs)     
+exp=5   # End-to-end loss-end-to-end      time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    training!  graham (def-l44xu-ab)
 
 
 
