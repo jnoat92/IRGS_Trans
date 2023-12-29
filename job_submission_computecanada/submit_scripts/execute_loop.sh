ad08489d479d10b62184263cee978baefe8aedb7
Noa-beluga <jnoat92@gmail.com>
2023-12-29 08:12:12 -0500

    s

--- job_submission_computecanada/submit_scripts/execute_loop.sh
+++ job_submission_computecanada/submit_scripts/execute_loop.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# exp=1   # multi-stage-cnn                 time --> 5.5 min per epoch (02:55:00 for 50 epochs)     training!  beluga (def-dclausi)
+# exp=1   # multi-stage-cnn                 time --> 5.5 min per epoch (02:55:00 for 50 epochs)     trained & tested!  beluga (def-dclausi)
 
-exp=2   # End-to-end loss-transformer     time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    training!  beluga (def-dclausi)
+# exp=2   # End-to-end loss-transformer     time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    trained & tested!  beluga (def-dclausi)
 
-# exp=3   # multi-stage-transformer         time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    !  beluga (def-dclausi)
+# exp=3   # multi-stage-transformer         time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    trained & tested!  beluga (def-l44xu-ab)
 
-# exp=4   # multi-stage-loss-end-to-end     time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    !  beluga (def-l44xu-ab) 
+# exp=4   # multi-stage-loss-end-to-end     time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    trained & tested!  beluga (def-dclausi) 
 
-# exp=5   # End-to-end loss-end-to-end      time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    training!  beluga (def-l44xu-ab) 
+# exp=5   # End-to-end loss-end-to-end      time --> 1.236hrs per epoch (65:00:00 for 50 epochs)    trained & tested!  beluga (def-l44xu-ab) 
 
 
-# train
-for i in {0..32}
-do
-    echo "executing train in model $i"
-    sbatch execute_train.sh $i 1 $exp
-    sleep 5
-done
+# # train
+# for i in {0..32}
+# do
+#     echo "executing train in model $i"
+#     sbatch execute_train.sh $i 1 $exp
+#     sleep 5
+# done
 
 # # test
 # for i in {0..32}
--- job_submission_computecanada/submit_scripts/execute_train.sh
+++ job_submission_computecanada/submit_scripts/execute_train.sh
@@ -4,9 +4,9 @@
 #SBATCH --tasks-per-node=4
 #SBATCH --cpus-per-task=8 # change this parameter to 2,4,6,... and increase "--num_workers" accordingly to see the effect on performance
 #SBATCH --mem=127510M
-#SBATCH --time=65:00:00
+#SBATCH --time=47:59:00
 #SBATCH --output=../output_logs/%j.out
-#SBATCH --account=def-dclausi
+#SBATCH --account=def-l44xu-ab
 #SBATCH --mail-user=jnoat92@gmail.com
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
